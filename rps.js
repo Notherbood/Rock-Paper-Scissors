bd3bec74c42013a77d7dd364f95830b9a6ab1439
Hiram <hmmartinezmarcano@gmail.com>
2022-10-27 20:05:12 -0500

    finished the function playround for the winning and losing outputs

--- rps.js
+++ rps.js
@@ -16,12 +16,27 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === "rock" && computerSelection === "scissor"){
     alert("You win! Rock beats scissor")
     }
+    else if (computerSelection === "scissor" && playerSelection === "paper"){
+        alert("You lose! Scissor beats Paper")
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock"){
+        alert("You lose! Paper beats Rock")
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissor"){
+        alert("You lose! Rock beats scissor")
+    }
+    else if (playerSelection === computerSelection){
+        alert("Draw!")
+    }
+    else {
+        alert("Please choose Rock, Paper or Sccisor")
+    }
 
 
   }
 
 
 const computerSelection = getComputerChoice();
-const playerSelection = "paper"
+const playerSelection = "rock"
 
 console.log(playRound(playerSelection, computerSelection));
