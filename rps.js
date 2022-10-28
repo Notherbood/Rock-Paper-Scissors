26aed9003ee694fe8aaeea9b2c3e62a5f216fbfd
Hiram <hmmartinezmarcano@gmail.com>
2022-10-27 20:20:10 -0500

    made an input for the player and changed the alerts to returns

--- rps.js
+++ rps.js
@@ -5,31 +5,34 @@ function getComputerChoice(){
     return choice = options[Math.floor(Math.random()* options.length)]
 }
 
+ playerInput = prompt("Choose Rock, Paper or Scissor")
+
+
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === "scissor" && computerSelection === "paper"){
-    alert("You win! Scissor beats Paper")
+        return("You win! Scissor beats Paper")
     }
     else if (playerSelection === "paper" && computerSelection === "rock"){
-    alert("You win! Paper beats Rock")
+        return("You win! Paper beats Rock")
     }
     else if (playerSelection === "rock" && computerSelection === "scissor"){
-    alert("You win! Rock beats scissor")
+        return("You win! Rock beats scissor")
     }
     else if (computerSelection === "scissor" && playerSelection === "paper"){
-        alert("You lose! Scissor beats Paper")
+        return("You lose! Scissor beats Paper")
     }
     else if (computerSelection === "paper" && playerSelection === "rock"){
-        alert("You lose! Paper beats Rock")
+        return("You lose! Paper beats Rock")
     }
     else if (computerSelection === "rock" && playerSelection === "scissor"){
-        alert("You lose! Rock beats scissor")
+        return("You lose! Rock beats scissor")
     }
     else if (playerSelection === computerSelection){
-        alert("Draw!")
+        return("Draw!")
     }
     else {
-        alert("Please choose Rock, Paper or Sccisor")
+        return("Please choose Rock, Paper or Sccisor")
     }
 
 
@@ -37,6 +40,5 @@ function playRound(playerSelection, computerSelection) {
 
 
 const computerSelection = getComputerChoice();
-const playerSelection = "rock"
-
+const playerSelection = playerInput.toLowerCase();
 console.log(playRound(playerSelection, computerSelection));
