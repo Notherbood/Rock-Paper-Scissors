4ae6f5bd18edce00f95d6065cf13f7f2ada0a565
Hiram <hmmartinezmarcano@gmail.com>
2022-11-01 20:54:53 -0600

    after breaking my head on the keyboard got the loop working, now i need to figure how to keep score

--- rps.js
+++ rps.js
@@ -4,29 +4,31 @@ function getComputerChoice(){
     const options = ["rock", "paper" , "scissor"]
     return choice = options[Math.floor(Math.random()* options.length)]
 }
-
- playerInput = prompt("Choose Rock, Paper or Scissor")
+function getPlayerChoice(){
+    const playerInput = prompt("Choose Rock, Paper or Scissor")
+    return choice = playerInput.toLowerCase();
+}
 
 
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === "scissor" && computerSelection === "paper"){
-        return("You win! Scissor beats Paper")
+        return("You win! Scissor beats Paper"); ++userScore; 
     }
     else if (playerSelection === "paper" && computerSelection === "rock"){
-        return("You win! Paper beats Rock")
+        return("You win! Paper beats Rock"); ++userScore
     }
     else if (playerSelection === "rock" && computerSelection === "scissor"){
-        return("You win! Rock beats scissor")
+        return("You win! Rock beats scissor"); ++userScore
     }
     else if (computerSelection === "scissor" && playerSelection === "paper"){
-        return("You lose! Scissor beats Paper")
+        return("You lose! Scissor beats Paper"); ++computerScore
     }
     else if (computerSelection === "paper" && playerSelection === "rock"){
-        return("You lose! Paper beats Rock")
+        return("You lose! Paper beats Rock"); ++computerScore
     }
     else if (computerSelection === "rock" && playerSelection === "scissor"){
-        return("You lose! Rock beats scissor")
+        return("You lose! Rock beats scissor"); ++computerScore
     }
     else if (playerSelection === computerSelection){
         return("Draw!")
@@ -34,11 +36,20 @@ function playRound(playerSelection, computerSelection) {
     else {
         return("Please choose Rock, Paper or Sccisor")
     }
+    
+}
 
+let userScore = 0;
+let computerScore = 0;
 
-  }
-
-
+function game(){
+    for (let i = 0; i < 5; i++){
 const computerSelection = getComputerChoice();
-const playerSelection = playerInput.toLowerCase();
+const playerSelection = getPlayerChoice();
 console.log(playRound(playerSelection, computerSelection));
+console.log("your score = " + userScore);
+console.log("Computer's score = " + computerScore);
+    }
+    
+}
+game()
